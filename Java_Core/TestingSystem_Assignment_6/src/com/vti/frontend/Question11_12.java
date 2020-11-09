01fd6eccba0f5944bf4a9abc188b3a9827bf0d6b
DESKTOP-A9KGAOK\AD <ducmin248@gmail.com>
2020-11-09 10:34:21 +0700

    update

---
+++ Java_Core/TestingSystem_Assignment_6/src/com/vti/frontend/Question11_12.java
@@ -0,0 +1,12 @@
+package com.vti.frontend;
+
+import com.vti.entity.Account;
+
+public class Question11_12 {
+    public static void main(String[] args) {
+        // account
+        System.out.println("Nhập thông tin account: ");
+        Account account = new Account();
+        System.out.println(account);
+    }
+}
