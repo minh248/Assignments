5ff3320ee933cbe072a532210f288859dba51b1e
DESKTOP-A9KGAOK\AD <ducmin248@gmail.com>
2021-02-01 17:57:33 +0700

    update

--- React/assignment_7/src/App.js
+++ React/assignment_7/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import EmployeeList from "./api/test";
+import EmployeeList from "./Ex1/EmployeeList";
 
 function App() {
     return (
